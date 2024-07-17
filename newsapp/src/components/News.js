d96f1e59b96efc18dd706fa7feb9c898a3b7b7c0
premlama12 <lprem0418@gmail.com>
2024-07-17 00:50:06 -0500

    loading

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -1,71 +1,81 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from 'react';
+import NewsItem from './NewsItem';
+import Spinner from './Spinner';
 
 export default class News extends Component {
-  
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      articles:[],
-      loading:false,
-      page:1
-    }
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+      totalResults: 0,
+    };
   }
 
-  async componentDidMount(){
-    let url = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=da00831589a24119a7596f8e9fc9e1cc&page=1&pagesize=20";
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=da00831589a24119a7596f8e9fc9e1cc&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true})
     let data = await fetch(url);
-    let parsedData = await data.json()
+    let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({articles: parsedData.articles,totalResults:parsedData.totalResults});
+    this.setState({ articles: parsedData.articles, 
+      totalResults: parsedData.totalResults,
+      loading:false 
+    });
   }
 
-  handlePrevClick =async ()=>{
-    
-    let url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=da00831589a24119a7596f8e9fc9e1cc${this.state.page - 1}&pagesize=20`;
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=da00831589a24119a7596f8e9fc9e1cc&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true})
     let data = await fetch(url);
-    let parsedData = await data.json()
+    let parsedData = await data.json();
     console.log(parsedData);
-    this.setState=({
+    this.setState({
       page: this.state.page - 1,
-      articles: parsedData.articles
-    })
+      articles: parsedData.articles,
+      loading:false
+    });
   }
 
-  handleNextClick =async ()=>{
-    if(this.state.page+1<Math.ceil(this.state.totalResults/20)){
-
+  handleNextClick = async () => {
+    if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
+      let url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=da00831589a24119a7596f8e9fc9e1cc&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      this.setState({loading:true})
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+        loading:false
+      });
     }
-    else{
-    let url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=da00831589a24119a7596f8e9fc9e1cc${this.state.page + 1}&pagesize=20`;
-    let data = await fetch(url);
-    let parsedData = await data.json()
-    console.log(parsedData);
-    this.setState=({
-      page: this.state.page + 1,
-      articles: parsedData.articles
-    })
-  }
   }
 
   render() {
     return (
       <div className="container my-3">
-        <h1>Latest News </h1>
+        <h1 className="text-center">Latest News</h1>
+         {this.state.loading && <Spinner/>}
         <div className="row">
-          {this.state.articles.map((element) => {
-            return <div className="col-md-4" key={element.url}>
-            <NewsItem  title={element.title?element.title.slice(0,45):""} 
-            description={element.description?element.description.slice(0,88):""} 
-            imageUrl={element.urlToImage} newsUrl={element.url}/>
-          </div>
+          {!this.state.loading && this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title.slice(0, 45) : ""}
+                  description={element.description ? element.description.slice(0, 88) : ""}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
           })}
         </div>
         <div className="container d-flex justify-content-between">
-        <button type="button" disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Preview</button>
-        <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+          <button type="button" disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+          <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
-    )
+    );
   }
 }
